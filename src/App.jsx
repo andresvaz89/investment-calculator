af5996d87febd7e6e1f9a91f9bafb887e6e8efed
Andres Vazquez <andres.vazquez.martin@gmail.com>
2024-01-07 18:44:19 +0100

    UserInput component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import Header from './component/Header';
+import UserInput from './component/UserInput';
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <UserInput />
+    </>
+  );
 }
 
 export default App;
