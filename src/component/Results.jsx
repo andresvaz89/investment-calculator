dbb0ded246e2ba70170b9478aa8b0c469f45639c
Andres Vazquez <andres.vazquez.martin@gmail.com>
2024-01-15 20:03:01 +0100

    added table withing Results component and its logic

--- src/component/Results.jsx
+++ src/component/Results.jsx
@@ -1,8 +1,44 @@
 import React from 'react';
+import { calculateInvestmentResults, formatter } from '../util/investment';
 
 const Results = ({ input }) => {
-  console.log(input);
-  return <p>Results</p>;
+  const resultsData = calculateInvestmentResults(input);
+  const initialInvestment =
+    resultsData[0].valueEndOfYear -
+    resultsData[0].interest -
+    resultsData[0].annualInvestment;
+  console.log(initialInvestment);
+  return (
+    <table id="result">
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Investment Value</th>
+          <th>Interest (year)</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+      <tbody>
+        {resultsData.map((yearData) => {
+          const totalInterest =
+            yearData.valueEndOfYear -
+            yearData.annualInvestment * yearData.year -
+            initialInvestment;
+          const totalAmountInvestment = yearData.valueEndOfYear - totalInterest;
+          return (
+            <tr>
+              <td>{yearData.year}</td>
+              <td>{formatter.format(yearData.valueEndOfYear)}</td>
+              <td>{formatter.format(yearData.interest)}</td>
+              <td>{formatter.format(totalInterest)}</td>
+              <td>{formatter.format(totalAmountInvestment)}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
 };
 
 export default Results;
